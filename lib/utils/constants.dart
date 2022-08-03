34104059876f85fe1507a3cd11dbbfcedea2bc4b
dazzlemon <daanill13470@gmail.com>
2022-08-04 00:34:20 +0300

    extension ChangeRoute

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,6 +3,15 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 final supabase = Supabase.instance.client;
 
+extension ChangeRoute on State {
+	void changeRoute(String newRoute) {
+		if (mounted) {
+			Navigator.of(context)
+			         .pushNamedAndRemoveUntil(newRoute, (route) => false);
+		}
+	}
+}
+
 extension ShowSnackBar on BuildContext {
   void showSnackBar({
     required String message,
